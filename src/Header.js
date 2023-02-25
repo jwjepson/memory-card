89be66ab9d8986a52107391cd35724e85fde57ce
Jake Jepson <jwjepson1@gmail.com>
2023-02-25 18:33:16 -0500

    insert logo to header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles/memory.css";
 import {ScoreBoard} from "./ScoreBoard";
+import logo from "./images/logo.png";
 
 const Header = () => {
     return (
         <div className="header">
+            <img src={logo} alt="Old School Runescape Logo"></img>
             <h1 className="title">Memory Game</h1>
         </div>
     );
