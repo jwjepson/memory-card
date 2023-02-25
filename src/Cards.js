5303876c63fd66695176d0e5eefcf8fc24ea97ea
Jake Jepson <jwjepson1@gmail.com>
2023-02-25 17:19:19 -0500

    create cards in state & loop through/shuffle on each turn

--- src/Cards.js
+++ src/Cards.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import { Card } from "./Card"
 import "./styles/memory.css";
 import rune from "./images/rune.png";
@@ -6,11 +6,65 @@ import gold from "./images/gold.png";
 import reaper from "./images/reaper.png";
 import wizardHat from "./images/wizardHat.png";
 import chicken from "./images/chicken.png";
-import {ScoreBoard} from "./ScoreBoard";
+import bones from "./images/bones.png";
+import dragonHelm from "./images/dragonHelm.png";
+import fireCape from "./images/fireCape.png";
+import spellBook from "./images/spellBook.png";
+import santaHat from "./images/santaHat.png";
 
 const Cards = (props) => {
+
+    const [cards, setCards] = useState([
+        {
+            name: "Rune",
+            img: rune,
+        },
+        {
+            name: "Gold",
+            img: gold,
+        },
+        {
+            name: "Grim Reaper",
+            img: reaper,
+        },
+        {
+            name: "Wizard Hat",
+            img: wizardHat,
+        },
+        {
+            name: "Chicken",
+            img: chicken,
+        },
+        {
+            name: "Bones",
+            img: bones,
+        },
+        {
+            name: "Dragon Helm",
+            img: dragonHelm,
+        },
+        {
+            name: "Fire Cape",
+            img: fireCape,
+        },
+        {
+            name: "Spell Book",
+            img: spellBook,
+        },
+        {
+            name: "Santa Hat",
+            img: santaHat,
+        }
+    ]);
     
+    const shuffleCards = () => {
+        const newArray = [...cards];
+        newArray.sort(() => Math.random() - 0.5);
+        setCards(newArray);
+    }
+
     const handleCardClick = (e) => {
+        shuffleCards();
         if (!props.selectedCards.includes(e.target.parentNode.dataset.name)) {
             props.setSelectedCards(props.selectedCards.concat(e.target.parentNode.dataset.name));
             props.setCurrentScore(props.currentScore + 1);
@@ -22,11 +76,9 @@ const Cards = (props) => {
     return (
         <>
             <div className="cards">
-                <Card handleCardClick={handleCardClick} img={rune} name="Rune"/>
-                <Card handleCardClick={handleCardClick} img={gold} name="Gold"/>
-                <Card handleCardClick={handleCardClick} img={reaper} name="Grim Reaper"/>
-                <Card handleCardClick={handleCardClick} img={wizardHat} name="Wizard Hat"/>
-                <Card handleCardClick={handleCardClick} img={chicken} name="Chicken"/>
+                {cards.map(card => (
+                    <Card handleCardClick={handleCardClick} img={card.img} name={card.name}/>
+                ))}
             </div>
         </>
     )
