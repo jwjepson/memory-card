f9de537f37c9723a2ec1a85844d1664238c6f02f
Jake Jepson <jwjepson1@gmail.com>
2023-02-25 16:19:38 -0500

    check if card has already been clicked and update game accordingly

--- src/Cards.js
+++ src/Cards.js
@@ -6,16 +6,29 @@ import gold from "./images/gold.png";
 import reaper from "./images/reaper.png";
 import wizardHat from "./images/wizardHat.png";
 import chicken from "./images/chicken.png";
+import {ScoreBoard} from "./ScoreBoard";
 
-const Cards = () => {
+const Cards = (props) => {
+    
+    const handleCardClick = (e) => {
+        if (!props.selectedCards.includes(e.target.parentNode.dataset.name)) {
+            props.setSelectedCards(props.selectedCards.concat(e.target.parentNode.dataset.name));
+            props.setCurrentScore(props.currentScore + 1);
+        } else {
+            props.resetGame()
+        }
+    }
+
     return (
-        <div className="cards">
-            <Card img={rune} name="Rune"/>
-            <Card img={gold} name="Gold"/>
-            <Card img={reaper} name="Grim Reaper"/>
-            <Card img={wizardHat} name="Wizard Hat"/>
-            <Card img={chicken} name="Chicken"/>
-        </div>
+        <>
+            <div className="cards">
+                <Card handleCardClick={handleCardClick} img={rune} name="Rune"/>
+                <Card handleCardClick={handleCardClick} img={gold} name="Gold"/>
+                <Card handleCardClick={handleCardClick} img={reaper} name="Grim Reaper"/>
+                <Card handleCardClick={handleCardClick} img={wizardHat} name="Wizard Hat"/>
+                <Card handleCardClick={handleCardClick} img={chicken} name="Chicken"/>
+            </div>
+        </>
     )
 }
 
