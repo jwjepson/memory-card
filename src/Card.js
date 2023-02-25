8d8b1c8426268528e5e904d371c698fe7139dd62
Jake Jepson <jwjepson1@gmail.com>
2023-02-25 16:18:28 -0500

    add onClick to card

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,9 @@ import React, {useState} from "react";
 import "./styles/memory.css";
 
 const Card = (props) => {
+
     return (
-        <div className="card">
+        <div onClick={props.handleCardClick} data-name={props.name} className="card">
             <img className="card-image" src={props.img} alt={props.name}></img>
             <div>{props.name}</div>
         </div>
