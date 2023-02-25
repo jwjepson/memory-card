ac5c0ca3626d2d0020142cc820f2c84ace815a66
Jake Jepson <jwjepson1@gmail.com>
2023-02-25 16:20:51 -0500

    create state for selectedCards, and scores, passing as props to child components

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles/memory.css";
 import {Cards} from "./Cards";
+import {ScoreBoard} from "./ScoreBoard";
 
 const Main = () => {
+    const [selectedCards, setSelectedCards] = useState([]);
+    const [currentScore, setCurrentScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
+
+    const resetGame = () => {
+        if (bestScore < currentScore) {
+            setBestScore(currentScore);
+        }
+        setCurrentScore(0);
+        setSelectedCards([]);
+    }
     return (
         <div className="main">
-            <Cards/>
+            <ScoreBoard currentScore={currentScore} bestScore={bestScore}/>
+            <Cards resetGame={resetGame} 
+            selectedCards={selectedCards} 
+            setSelectedCards={setSelectedCards} 
+            setCurrentScore={setCurrentScore} 
+            currentScore={currentScore} 
+            bestScore={bestScore}
+            setBestScore={setBestScore}/>
         </div>
     )
 }
