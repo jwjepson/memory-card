40eef704c496f62a2cf80c391083bf6896aef4c6
Jake Jepson <jwjepson1@gmail.com>
2023-02-24 20:54:43 -0500

    add title and scoreboard to header

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./styles/memory.css";
+import {ScoreBoard} from "./ScoreBoard";
 
 const Header = () => {
     return (
-        <div className="header"></div>
+        <div className="header">
+            <h1 className="title">Memory Game</h1>
+            <ScoreBoard/>
+        </div>
     );
 }
 
