1e7e6061954f9b9a057b38e8bd1da43f14fb0231
Jake Jepson <jwjepson1@gmail.com>
2023-02-25 20:41:48 -0500

    add card title class

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ const Card = (props) => {
     return (
         <div onClick={props.handleCardClick} data-name={props.name} className="card">
             <img className="card-image" src={props.img} alt={props.name}></img>
-            <div>{props.name}</div>
+            <div className="card-title">{props.name}</div>
         </div>
     )
 }
