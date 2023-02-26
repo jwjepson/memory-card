c131733f47747290397457433693f9129e1f0bc9
Jake Jepson <jwjepson1@gmail.com>
2023-02-25 21:13:49 -0500

    put state inside app component and pass as props to children

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import { Header } from "./Header";
 import {Main} from "./Main";
 import { Footer } from "./Footer";
 import "./styles/memory.css";
+import React, {useState} from "react";
 
 function App() {
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  const resetGame = () => {
+      if (bestScore < currentScore) {
+          setBestScore(currentScore);
+      }
+      setCurrentScore(0);
+      setSelectedCards([]);
+  };
+
   return (
     <div className="app">
-      <Header/>
-      <Main/>
+      <Header currentScore={currentScore} bestScore={bestScore}/>
+      <Main resetGame={resetGame} 
+            selectedCards={selectedCards} 
+            setSelectedCards={setSelectedCards} 
+            setCurrentScore={setCurrentScore} 
+            currentScore={currentScore} 
+            bestScore={bestScore}
+            setBestScore={setBestScore}/>
       <Footer/>
     </div>
   );
