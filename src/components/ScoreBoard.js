63a9d3147bafc9c7327c8ed1aa43abed508b14fc
Jake Jepson <jwjepson1@gmail.com>
2023-02-25 21:37:12 -0500

    move scoreboard component in components folder

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
-import scoreBackground from "./images/scroll-background.png";
+import React from "react";
+import scoreBackground from "../images/scroll-background.png";
 
 const ScoreBoard = (props) => {
     return (
         <div className="scoreboard">
-            <img className="score-background" src={scoreBackground}></img>
+            <img className="score-background" alt="ScoreBoard scroll background" src={scoreBackground}></img>
             <div className="current-score">Score: {props.currentScore}</div>
             <div className="best-score">Best Score: {props.bestScore}</div>
         </div>
