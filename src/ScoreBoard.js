c3de6b0072a51a937e69699ba7d4bf30ab649ab4
Jake Jepson <jwjepson1@gmail.com>
2023-02-25 20:43:41 -0500

    import scoreboard background

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
+import scoreBackground from "./images/scroll-background.png";
 
 const ScoreBoard = (props) => {
     return (
         <div className="scoreboard">
+            <img className="score-background" src={scoreBackground}></img>
             <div className="current-score">Score: {props.currentScore}</div>
             <div className="best-score">Best Score: {props.bestScore}</div>
         </div>
