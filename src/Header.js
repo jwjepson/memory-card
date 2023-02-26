3c47d14535c34936393ca68a6741f03475c0ee04
Jake Jepson <jwjepson1@gmail.com>
2023-02-25 21:14:09 -0500

    put the scoreboard inside the header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./styles/memory.css";
 import logo from "./images/logo.png";
+import { ScoreBoard } from "./ScoreBoard";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <div className="header">
-            <img src={logo} alt="Old School Runescape Logo"></img>
-            <h1 className="title">Memory Game</h1>
+            <div className="header-info">
+                <img src={logo} alt="Old School Runescape Logo"></img>
+                <h1 className="title">Memory Game</h1>
+            </div>
+            <ScoreBoard currentScore={props.currentScore} bestScore={props.bestScore}/>
         </div>
     );
 }
